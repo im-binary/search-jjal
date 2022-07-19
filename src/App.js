e9069653d7cbc7a8d99959adb511b98f6ffe1fa8
pongdang <pongdang0121@gmail.com>
2022-07-19 11:32:31 +0900

    feat: 컨텐츠 가운데 정렬

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import styled from "styled-components";
 import SearchList from "./pages/SearchList";
 import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <>
+    <SectionAppContainer>
       <GlobalStyle />
       <SearchList />
-    </>
+    </SectionAppContainer>
   );
 }
+
+const SectionAppContainer = styled.section`
+  display: flex;
+  justify-content: center;
+  padding-top: 20px;
+`;
 
 export default App;
