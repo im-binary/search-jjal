0e1a0ec08a60ee349d7d0f905a908913ca2133c5
pongdang <pongdang0121@gmail.com>
2022-07-19 11:31:03 +0900

    feat: 로딩 컴포넌트

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import styled from "styled-components";
 
 export default function Loading() {
-  return <div>Loading...</div>;
+  return <LoadingSpace>Loading...</LoadingSpace>;
 }
+
+const LoadingSpace = styled.article`
+  width: 300px;
+  height: 300px;
+  border: 1px solid;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
