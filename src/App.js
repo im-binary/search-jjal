e0289d3889aace39752b1b2f1593d9c048ab407c
pongdang <pongdang0121@gmail.com>
2022-07-19 10:21:39 +0900

    feat: 글로벌 스타일 컴포넌트 사용

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import SearchList from "./pages/SearchList";
+import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <SearchList />
     </>
   );
