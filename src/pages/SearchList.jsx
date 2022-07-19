541eb9c595f932d6069a9bc0dca4ae1d1ae9eb8a
pongdang <pongdang0121@gmail.com>
2022-07-19 10:21:17 +0900

    chore: api 함수 따로 빼기

--- src/pages/SearchList.jsx
+++ src/pages/SearchList.jsx
@@ -1,42 +1,67 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import styled from "styled-components";
 
 export default function SearchList() {
   const [list, setList] = useState([]);
   const [keyword, setKeyword] = useState("cat");
 
-  useEffect(() => {
-    axios
-      .get(`https://api.giphy.com/v1/gifs/search?api_key=A1XrfRGc2MX7wmZktzh08ZucZJztvS7E&q=${keyword}`)
-      .then((res) => {
-        setList(res.data.data);
-      });
-    return () => {};
-  }, [keyword]);
+  const gifAPIurl = `https://api.giphy.com/v1/gifs/search?api_key=A1XrfRGc2MX7wmZktzh08ZucZJztvS7E&q=${keyword}`;
 
-  const handleClick = () => {
-    axios
-      .get(`https://api.giphy.com/v1/gifs/search?api_key=A1XrfRGc2MX7wmZktzh08ZucZJztvS7E&q=${keyword}`)
-      .then((res) => {
-        setList(res.data.data);
-      });
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const gifApi = async () => {
+    const response = await axios.get(gifAPIurl);
+    setList(response.data.data);
   };
+
+  useEffect(() => {
+    gifApi();
+  }, [gifApi, keyword]);
+
   const handleChange = (e) => {
     const value = e.target.value;
     setKeyword(value);
   };
+
   return (
     <section>
       <label htmlFor='searchKeyword'></label>
-      <input name='searchKeyword' type='text' onChange={handleChange} />
-      <button onClick={handleClick}>검색</button>
-      <ul>
+      <InputSearch name='searchKeyword' type='text' onChange={handleChange} />
+      <ButtonSearch onClick={gifApi}>검색</ButtonSearch>
+      <UlGifListContainer>
         {list.map((item, i) => (
-          <li>
+          <li key={item.id}>
             <img src={item.images.original.url} key={item.id} alt={item.title} />
           </li>
         ))}
-      </ul>
+      </UlGifListContainer>
     </section>
   );
 }
+
+const UlGifListContainer = styled.ul`
+  list-style: none;
+  padding: 0;
+  img {
+    width: 300px;
+  }
+`;
+
+const InputSearch = styled.input`
+  width: 300px;
+  height: 30px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0 10px;
+  margin-bottom: 10px;
+`;
+
+const ButtonSearch = styled.button`
+  width: 100px;
+  height: 30px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 0 10px;
+  margin-bottom: 10px;
+  cursor: pointer;
+`;
