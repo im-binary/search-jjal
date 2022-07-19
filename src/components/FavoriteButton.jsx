845baf8d55b5d84958a2a92d9d64da47cabe8c8e
pongdang <pongdang0121@gmail.com>
2022-07-19 15:35:55 +0900

    chore: 필요없는 매개변수 제거

--- src/components/FavoriteButton.jsx
+++ src/components/FavoriteButton.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function FavoriteButton({ imgSrc, favorite, setFavorite }) {
-  const handleHeartClick = (e) => {
+  const handleHeartClick = () => {
     setFavorite([...favorite, imgSrc]);
 
     if (favorite.includes(imgSrc)) {
