ac2273a534d77f67314aca63489808928bedf748
pongdang <pongdang0121@gmail.com>
2022-08-17 11:11:19 +0900

    feat: masonry 레이아웃

--- src/pages/SearchList.jsx
+++ src/pages/SearchList.jsx
@@ -8,7 +8,7 @@ import FavoriteButton from "../components/FavoriteButton";
 export default function SearchList() {
   const [list, setList] = useState([]);
   const [keyword, setKeyword] = useState("cat");
-  const [favorite, setFavorite] = useState([]);
+  const [favorite, setFavorite] = useState(JSON.parse(localStorage.getItem("favorite")) || []);
 
   const gifAPIurl = `https://api.giphy.com/v1/gifs/search?api_key=A1XrfRGc2MX7wmZktzh08ZucZJztvS7E&q=${keyword}&limit=20`;
 
@@ -45,16 +45,21 @@ export default function SearchList() {
 const UlGifListContainer = styled.ul`
   list-style: none;
   padding: 0;
-  padding-top: 50px;
-  display: grid;
+  margin-top: 10px;
+  /* display: grid;
   grid-template-columns: 1fr 1fr 1fr;
   justify-items: center;
-  align-items: center;
-  gap: 15px;
+  align-items: flex-start;
+  gap: 15px; */
+  column-count: 3;
 
   li {
+    display: inline-flex;
+    flex-direction: column;
+    width: 100%;
     background: #eeecf3;
     padding: 20px;
+    margin-bottom: 10px;
     border-radius: 10px;
     box-shadow: 3px 3px 10px rgb(0 0 0 / 36%);
     transition: 500ms ease-in-out;
@@ -62,15 +67,17 @@ const UlGifListContainer = styled.ul`
 
   li:hover {
     transition: 500ms ease-in-out;
-    transform: scale(1.1);
+    transform: scale(1.03);
   }
 
   @media (max-width: 926px) {
-    grid-template-columns: 1fr 1fr;
-    gap: 15px;
+    /* grid-template-columns: 1fr 1fr; */
+    /* gap: 15px; */
+    column-count: 2;
   }
 
   @media (max-width: 634px) {
-    grid-template-columns: 1fr;
+    /* grid-template-columns: 1fr; */
+    column-count: 1;
   }
 `;
