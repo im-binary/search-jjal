eb80e2e1cad873327686fe743bc15b272e9b485a
pongdang <pongpong0121@kakao.com>
2022-11-23 19:16:00 +0900

    feat: 검색 시 보여줄 로딩 화면

--- src/pages/SearchListPage.jsx
+++ src/pages/SearchListPage.jsx
@@ -2,6 +2,7 @@ import React, { Suspense } from "react";
 import SearchInput from "../components/SearchInput";
 import qs from "qs";
 import { GifList } from "../components/GifList";
+import { SearchLoading } from "../components/SearchLoading";
 
 export function SearchListPage() {
   const { q } = qs.parse(window.location.search.slice(1));
@@ -26,7 +27,7 @@ export function SearchListPage() {
         />
       ) : null} */}
 
-      <Suspense fallback={<>로딩 중</>}>
+      <Suspense fallback={<SearchLoading keyword={q ?? "cat"} />}>
         <GifList keyword={q ?? "cat"} />
       </Suspense>
     </article>
