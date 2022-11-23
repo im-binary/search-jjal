97ccd8a1f53758f8d1e0e3331296bb19078e7217
pongdang <pongpong0121@kakao.com>
2022-11-23 17:32:25 +0900

    feat: 로딩 부분 css 수정

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -2,13 +2,35 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Loading({ width = 300 }) {
-  return <LoadingSpace width={width}>Loading...</LoadingSpace>;
+  return (
+    <LoadingSpace>
+      <p>Loading...</p>
+    </LoadingSpace>
+  );
 }
 
 const LoadingSpace = styled.article`
-  width: ${(props) => props.width}px;
   height: 100%;
   display: flex;
   align-items: center;
   justify-content: center;
+
+  p {
+    animation: biggerAndSmaller 2s linear infinite;
+    letter-spacing: 0.2em;
+  }
+
+  @keyframes biggerAndSmaller {
+    0% {
+      transform: scale(1);
+    }
+
+    50% {
+      transform: scale(1.2);
+    }
+
+    100% {
+      transform: scale(1);
+    }
+  }
 `;
