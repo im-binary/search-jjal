c1b8e58214f852283e7f9ab0b17325a0f104fe77
pongdang <pongpong0121@kakao.com>
2022-11-23 18:58:41 +0900

    feat: font 적용

--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,13 +1,22 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
+   @font-face {
+    font-family: 'Cafe24Ssurround';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2105_2@1.0/Cafe24Ssurround.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
+}
+
     * {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        font-family: 'Cafe24Ssurround';
     }
+
     body {
-        background: linear-gradient(90deg, #624479, #1f043d);
+        background: linear-gradient(180deg, #ffffff, #1f043d);
     }
 `;
 
